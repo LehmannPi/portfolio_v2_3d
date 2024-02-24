a59a49040186418fbe22c5a4d98d1bc4b8b3acbd
Filipe <filipelehmannp@gmail.com>
2024-02-24 19:31:31 -0300

    feat: main app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  StarsCanvas,
+  Tech,
+  Works,
+} from './components';
 
 const App = () => {
   return (
@@ -7,6 +18,16 @@ const App = () => {
         <div className="bg-center bg-no-repeat bg-cover bg-hero-pattern">
           <Navbar />
           <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
